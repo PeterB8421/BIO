6a7b39c2cc619d8d67cce25e0484310e69a90e15
Petr Balok <petrbalok@gmail.com>
2023-11-13 14:55:37 +0100

    Fixed a few imperfections with saving to json files.

--- CompareFace_scripts/compare_results.py
+++ CompareFace_scripts/compare_results.py
@@ -2,6 +2,7 @@ import json
 import os.path
 import sys
 import glob
+import re
 
 from compreface import CompreFace
 from compreface.service import VerificationService
@@ -15,23 +16,45 @@ compre_face: CompreFace = CompreFace(DOMAIN, PORT)
 verify: VerificationService = compre_face.init_face_verification(API_KEY)
 
 
+def create_json_directories(directory):
+    for root, dirs, files in os.walk(directory):
+        json_dir = os.path.join(root, 'json')
+        if not os.path.exists(json_dir):
+            os.makedirs(json_dir)
+
+
 def main():
     if len(sys.argv) != 2:
         print('Usage: python compare_results.py <dir>\n'
               '<dir> = Directory containing subdirectories with files ending as "_result.jpg"', file=sys.stderr)
         exit(1)
     path = sys.argv[1]
-    files = glob.glob(os.path.join(path, '*/*result.jpg'))
+    json_dirs = glob.glob(os.path.join(path, '*/json'))
+    if not json_dirs:
+        create_json_directories(path)
+    files = glob.glob(os.path.join(path, '*/*_result.jpg'))
     results = []
     for src in files:
+        prefix = re.search(r'\\(\d+)\\', src)
+        prefix = prefix.group(1)
         for target in files:
             if target == src:
                 continue
-            results.append(verify.verify(src, target, options={
+            target_prefix = re.search(r'\\(\d+)\\', target)
+            target_prefix = target_prefix.group(1)
+            if prefix != target_prefix:
+                continue
+            current = verify.verify(src, target, options={
                 "face_plugins": "landmarks",
-            }))
-        with open(src.replace('.jpg', '.json'), 'w') as json_file:
-            json.dump(results, json_file, indent=4)
+            })
+            current['src'] = src
+            current['target'] = target
+            results.append(current)
+        filename = os.path.basename(src)
+        json_path = os.path.join(path, prefix, 'json', f'{filename.replace(".jpg", ".json")}')
+        with open(json_path, 'w') as json_file:
+            json.dump(results, json_file, indent=2)
+        results = []
 
 
 if __name__ == '__main__':
